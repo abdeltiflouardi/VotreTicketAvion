6c84f1c2a2dd63caabafbbc22eb53ed62a567631
ouardisoft <louardi.abdeltif@yahoo.fr>
2011-09-21 22:14:52 +0000

    Required fields

--- src/Backend/CoreBundle/Form/ClientType.php
+++ src/Backend/CoreBundle/Form/ClientType.php
@@ -10,14 +10,18 @@ class ClientType extends AbstractType
     public function buildForm(FormBuilder $builder, array $options)
     {
         $builder
-            ->add('titre', 'choice', array('required' => false, 'choices' => array('' => '', 'Mr.' => 'Mr.', 'Ms.' => 'Ms.')))
-            ->add('nom')
-            ->add('prenom')
-            ->add('nationalite')
-            ->add('paysResidence')
-            ->add('gsm')
-            ->add('email')
+            ->add('titre', 'choice', array('required' => true, 'choices' => array('' => '', 'Mr.' => 'Mr.', 'Ms.' => 'Ms.')))
+            ->add('nom', 'text', array('required' => true))
+            ->add('prenom', 'text', array('required' => true))
+            ->add('nationalite', 'text', array('required' => true))
+            ->add('paysResidence', 'text', array('required' => true))
+            ->add('gsm', 'text', array('required' => true))
+            ->add('email', 'text', array('required' => true))
         ;
+    }
+    
+    public function getDefaultOptions(array $options) {
+        return array();
     }
 
     public function getName()
